174f4e1469f4f9f0b1b90b95054e08271edca9fe
ddvp <ddvp0188@gmail.com>
2023-04-20 20:47:46 -0600

    add hello world selenium

---
+++ Intro/hello_world.py
@@ -0,0 +1,13 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+chrome_driver_path = "./drivers/chromedriver.exe"
+url = "https://qamindslab.com/"
+
+
+service = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=service)
+driver.get(url)
+time.sleep(3)
+driver.quite()
