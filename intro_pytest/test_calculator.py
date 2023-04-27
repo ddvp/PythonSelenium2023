1b1e055301daf7f56c74ff092d8c60378c5e43c1
ddvp <ddvp0188@gmail.com>
2023-04-26 21:17:46 -0600

    test suite

--- intro_pytest/test_calculator.py
+++ intro_pytest/test_calculator.py
@@ -13,26 +13,25 @@ class Calculadora:
        return num_a // num_b
 
 
-    def test_suma():
+def test_suma():
         calc = Calculadora()
         result = calc.suma(2, 2)
         assert result == 4, "La suma de los numeros es correcta"
 
-    def test_resta():
+def test_resta():
         calc = Calculadora()
         result = calc.resta(2, 2)
         assert result == 0, "La resta de los numeros es correcta"
 
-    def test_multiplicacion():
+def test_multiplicacion():
         calc = Calculadora()
         result = calc.multiplicacion(2,5)
         assert result == 10, "La multiplicacion de los numeros es correcta"
 
-    def test_division():
+def test_division():
         calc = Calculadora()
         result = calc.division (4,2)
         assert result == 2.5, "La division de los numeros es correcta"
-
 
 
 
