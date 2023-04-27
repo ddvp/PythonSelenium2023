22558ff9a12e6ece451ca2309d1bee0a57bfc7ba
ddvp <ddvp0188@gmail.com>
2023-04-26 19:38:46 -0600

    test calculator

--- intro_pytest/test_calculator.py
+++ intro_pytest/test_calculator.py
@@ -1,20 +1,16 @@
 import pytest
+
 class Calculadora:
    def __init__(self):
        pass
-
    def suma(self, num_a: int, num_b: int):
        return num_a + num_b
-
    def resta(self, num_a: int, num_b: int):
        return num_a + num_b
-
    def multiplicacion(self, num_a: int, num_b: int):
        return num_a ** num_b
-
    def division(self, num_a: int, num_b: int):
        return num_a // num_b
-
 
 
     def test_suma():
