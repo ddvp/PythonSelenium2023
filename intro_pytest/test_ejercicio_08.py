6c06a445d1b673cd24dcea268b2581bc7ca858fc
ddvp <ddvp0188@gmail.com>
2023-06-09 11:30:21 -0600

    practicas update

--- intro_pytest/test_ejercicio_08.py
+++ intro_pytest/test_ejercicio_08.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 CHROME_DRIVER_PATH = "./drivers/chromedriver.exe"
 CHROME_SERVICE = Service(CHROME_DRIVER_PATH)
-URL = "https://demo.seleniumeasy.com/bootstrap-download-progress-demo.html"
+URL = "https://demo.seleniumeasy.com/bootstrap-download-progress-demo.html "
 
 
 class TestLandingPage:
@@ -20,21 +21,21 @@ class TestLandingPage:
 
     def test_download(self):
         # Obtener el elemento, y el estado importa
-        button = self.__find_clickable_element(By.ID, "autoclosable-btn-success")
-        label = self.__find_visible_element(By.ID, "autoclosable-btn-success")
+        download = self.__find_clickable_element(By.ID, "cricle-btn")
+        download.click()
+        self.__find_by_text(By.CLASS_NAME, "percenttext", "100%")
 
-    def __find_clickable_element(self, by: By, value: str):
+    def __find_clickable_element(self, by: By, value: str) -> WebElement:
         return self.wait_driver.until(EC.element_to_be_clickable((by, value)))
 
-    def __find_visible_element(self, by: By, value: str):
+    def __find_visible_element(self, by: By, value: str) -> WebElement:
         return self.wait_driver.until(EC.visibility_of_element_located((by, value)))
 
-    def __find_by_text(self, by: By, value: str, text: str):
+    def __find_by_text(self, by: By, value: str, text: str) -> WebElement:
         return self.wait_driver.until(EC.text_to_be_present_in_element((by, value), text))
 
     def __wait_until_disappears(self, by: By, value: str):
         self.wait_driver.until(EC.invisibility_of_element((by, value)))
-
 
     def teardown_method(self):
         self.driver.quit()
